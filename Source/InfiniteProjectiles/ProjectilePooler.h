fdcde4f8f7d0bc2e8b24345a865f68a9984f6116
EverettHanke <forgetekbusiness@gmail.com>
2025-11-18 21:39:32 -0800

    projectile pooler properties and functions

--- Source/InfiniteProjectiles/ProjectilePooler.h
+++ Source/InfiniteProjectiles/ProjectilePooler.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "PooledObject.h"
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "ProjectilePooler.generated.h"
@@ -16,9 +17,27 @@ public:
 	// Sets default values for this component's properties
 	UProjectilePooler();
 
+	UFUNCTION(BlueprintCallable, Category = "Pooling")
+		APooledObject* SpawnPooledObject();	
+
+	UPROPERTY(EditAnywhere, Category = "Pooling")
+		TSubclassOf<class APooledObject> PooledObjectSubclass;
+	
+	UPROPERTY(EditAnywhere, Category = "Pooling")
+		int PoolSize = 20;
+	
+	UPROPERTY(EditAnywhere, Category = "Pooling")
+		float PooledObjectLifeSpan = 0.0f;
+
+	UFUNCTION()
+		void OnPooledObjectDespawn(APooledObject* PoolActor);
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	TArray<APooledObject*> ObjectPool;
+	TArray<int> SpawnedPoolIndices;
 
 public:	
 	// Called every frame
