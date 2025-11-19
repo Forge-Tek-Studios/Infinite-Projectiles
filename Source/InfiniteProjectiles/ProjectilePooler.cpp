16b91d9dfa1ef1a5b19629b235885b1dc7a3cbf8
EverettHanke <forgetekbusiness@gmail.com>
2025-11-18 21:57:55 -0800

    setting up begin play for projectile pooler

--- Source/InfiniteProjectiles/ProjectilePooler.cpp
+++ Source/InfiniteProjectiles/ProjectilePooler.cpp
@@ -19,8 +19,56 @@ void UProjectilePooler::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	if (PooledObjectSubclass != nullptr)
+	{
+		UWorld* const World = GetWorld();
+		if (World != nullptr)
+		{
+			for (int i = 0; i < PoolSize; i++)
+			{
+				APooledObject* PooledObj = World->SpawnActor<APooledObject>(PooledObjectSubclass, FVector().ZeroVector, FRotator().ZeroRotator);
+				if (PooledObj != nullptr)
+				{
+					PooledObj->SetActive(false);
+					PooledObj->SetPoolIndex(i);
+					PooledObj->OnPooledObjectDespawn.AddDynamic(this, &UProjectilePooler::OnPooledObjectDespawn);
+					ObjectPool.Add(PooledObj);
+				}
+			}
+		}
+	}
 	
+}
+
+APooledObject* UProjectilePooler::SpawnPooledObject()
+{
+	for (int i = 0; i < ObjectPool.Num(); i++)
+	{
+		if (!SpawnedPoolIndices.Contains(i))
+		{
+			APooledObject* PooledObj = ObjectPool[i];
+			if (PooledObj)
+			{
+				PooledObj->SetLifeSpan(PooledObjectLifeSpan);
+				PooledObj->SetActorHiddenInGame(false);
+				PooledObj->SetActorEnableCollision(true);
+				SpawnedPoolIndices.Add(i);
+				return PooledObj;
+			}
+		}
+	}
+	return nullptr;
+}
+
+void UProjectilePooler::OnPooledObjectDespawn(APooledObject* PoolActor)
+{
+	int Index = ObjectPool.IndexOfByKey(PoolActor);
+	if (Index != INDEX_NONE)
+	{
+		SpawnedPoolIndices.Remove(Index);
+		PoolActor->SetActorHiddenInGame(true);
+		PoolActor->SetActorEnableCollision(false);
+	}
 }
 
 
