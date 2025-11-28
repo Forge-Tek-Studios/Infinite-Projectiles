89d6199f8ff0a24ede4b8a57ff79fa1e9a0e3ba5
EverettHanke <forgetekbusiness@gmail.com>
2025-11-27 17:18:15 -0800

    adding component actor for network replication wrapper purposes

---
+++ Source/InfiniteProjectiles/InfiniteProjectilesComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InfiniteProjectilesComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class INFINITEPROJECTILES_API UInfiniteProjectilesComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInfiniteProjectilesComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
